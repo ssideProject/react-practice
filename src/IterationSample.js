5bcb6bb9aff9a1adfef415b9671b991499a3ed2e
sstoneju <sstoneju@gmail.com>
2020-07-04 08:30:00 +0000

    fix: onRemove

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -24,7 +24,18 @@ const IterationSample = () => {
         setInputText('');
     };
 
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
+
+    const namesList = names.map(name => (
+        // USE closures
+        <li key={name.id} onDoubleClick={()=>onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+
     return(
         <>
             <input value={inputText} onChange={onChange}/>
